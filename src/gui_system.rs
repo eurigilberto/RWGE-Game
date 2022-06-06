7a27c184d8a9d41508817304173ca2537fb8129f
Gilberetzu <euri.herasme@gmail.com>
2022-06-06 00:14:40 -0400

    Added a render function implementation to the GUI system (not tested) Removed ownership of the GUIRects fromt he GUISystem The surface texture is now cleared on the runtime render function

--- src/gui_system.rs
+++ src/gui_system.rs
@@ -1,11 +1,20 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, mem::discriminant};
 
 use rwge::{
-    glam::{UVec2, uvec2},
-    gui::rect_ui::{slotmap::{GUIContainer, GUISlotmaps}, system::GUIRects, event::UIEvent}, slotmap::slotmap::{Slotmap, SlotKey}, EngineDataSlotmapTypes,
+    entity_component::{EngineDataTypeKey, PublicDataCollection},
+    glam::{uvec2, UVec2},
+    gui::rect_ui::{
+        event::UIEvent,
+        slotmap::{GUIContainer, GUISlotmaps},
+        system::GUIRects,
+    },
+    slotmap::slotmap::{SlotKey, Slotmap},
+    wgpu,
+    winit::window,
+    EngineDataType,
 };
 
-use crate::DataSlotmap;
+use crate::{DataType, DataTypeKey};
 
 pub struct WindowOne {
     pub size: UVec2,
@@ -19,7 +28,7 @@ pub struct WindowTwo {
     pub value: f32,
 }
 
-impl GUIContainer<DataSlotmap> for WindowOne {
+impl GUIContainer<DataTypeKey, DataType> for WindowOne {
     fn get_name(&self) -> &String {
         &self.name
     }
@@ -34,14 +43,14 @@ impl GUIContainer<DataSlotmap> for WindowOne {
 
     fn update(
         &self,
-        event: &rwge::gui::rect_ui::event::UIEvent,
-        public_data: &mut Slotmap<DataSlotmap>,
+        event: &UIEvent,
+        public_data: &mut rwge::entity_component::PublicDataCollection<DataTypeKey, DataType>,
     ) {
-        
+        todo!()
     }
 }
 
-impl GUIContainer<DataSlotmap> for WindowTwo {
+impl GUIContainer<DataTypeKey, DataType> for WindowTwo {
     fn get_name(&self) -> &String {
         &self.name
     }
@@ -56,66 +65,131 @@ impl GUIContainer<DataSlotmap> for WindowTwo {
 
     fn update(
         &self,
-        event: &rwge::gui::rect_ui::event::UIEvent,
-        public_data: &mut Slotmap<DataSlotmap>,
+        event: &UIEvent,
+        public_data: &mut rwge::entity_component::PublicDataCollection<DataTypeKey, DataType>,
     ) {
-        
+        todo!()
     }
 }
 
-pub enum WindowType{
-    WindowOne(GUISlotmaps<DataSlotmap, WindowOne>),
-    WindowTwo(GUISlotmaps<DataSlotmap, WindowTwo>)
+pub enum WindowType {
+    WindowOne(GUISlotmaps<DataTypeKey, DataType, WindowOne>),
+    WindowTwo(GUISlotmaps<DataTypeKey, DataType, WindowTwo>),
 }
 
-#[derive(Hash, PartialEq, Eq)]
-pub enum WindowTypeKey{
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+pub enum WindowTypeKey {
     WindowOne,
-    WindowTwo
+    WindowTwo,
 }
 
-pub struct WindowKey{
+pub struct WindowKey {
     pub map_key: WindowTypeKey,
-    pub key: SlotKey
+    pub key: SlotKey,
 }
 
 /// This version of the window system is only going to work with windowed spaces. This needs to be refactored in the future to support docking.
 pub struct GUISystem {
-    pub gui_rects: GUIRects,
     pub active_window_collection: Vec<(WindowTypeKey, SlotKey)>,
-    pub window_collection: HashMap<WindowTypeKey, WindowType>
+    pub window_collection: HashMap<WindowTypeKey, WindowType>,
+}
+
+fn update_window(
+    window_collection: &mut HashMap<WindowTypeKey, WindowType>,
+    gui_type: WindowTypeKey,
+    slot_key: SlotKey,
+    event: &UIEvent,
+    public_data: &mut PublicDataCollection<DataTypeKey, DataType>,
+) {
+    let window_slotmap = window_collection.get_mut(&gui_type).unwrap();
+    match window_slotmap {
+        WindowType::WindowOne(slotmap) => slotmap
+            .get_value_mut(slot_key)
+            .expect("Window one could not be retrieved")
+            .update(&event, public_data),
+        WindowType::WindowTwo(slotmap) => slotmap
+            .get_value_mut(slot_key)
+            .expect("Window two could not be retrieved")
+            .update(&event, public_data),
+    }
 }
 
 impl GUISystem {
-    pub fn new(gui_rects: GUIRects)->Self{
+    pub fn new() -> Self {
+        let window_one_sm = GUISlotmaps::<DataTypeKey, DataType, WindowOne>::new_with_capacity(20);
+        let window_two_sm = GUISlotmaps::<DataTypeKey, DataType, WindowTwo>::new_with_capacity(20);
 
-        let window_one_sm = GUISlotmaps::<DataSlotmap, WindowOne>::new_with_capacity(20);
-        let window_two_sm = GUISlotmaps::<DataSlotmap, WindowTwo>::new_with_capacity(20);
+        let mut window_collection: HashMap<WindowTypeKey, WindowType> =
+            HashMap::<WindowTypeKey, WindowType>::new();
+        window_collection.insert(
+            WindowTypeKey::WindowOne,
+            WindowType::WindowOne(window_one_sm),
+        );
+        window_collection.insert(
+            WindowTypeKey::WindowTwo,
+            WindowType::WindowTwo(window_two_sm),
+        );
 
-        let mut window_collection: HashMap<WindowTypeKey, WindowType> = HashMap::<WindowTypeKey, WindowType>::new();
-        window_collection.insert(WindowTypeKey::WindowOne, WindowType::WindowOne(window_one_sm));
-        window_collection.insert(WindowTypeKey::WindowTwo, WindowType::WindowTwo(window_two_sm));
+        let active_window_collection: Vec<(WindowTypeKey, SlotKey)> =
+            Vec::<(WindowTypeKey, SlotKey)>::with_capacity(20);
 
-        let active_window_collection: Vec<(WindowTypeKey, SlotKey)> = Vec::<(WindowTypeKey, SlotKey)>::with_capacity(20);
-
-        Self{
-            gui_rects,
+        Self {
             active_window_collection,
             window_collection,
         }
     }
 
-    pub fn handle_event(&mut self, event: &UIEvent, public_data: &mut Slotmap<DataSlotmap>){
-        for (window_type, slot_key) in self.active_window_collection.iter(){
-            let window_slotmap = self.window_collection.get_mut(window_type).unwrap();
-            match window_slotmap {
-                WindowType::WindowOne(slotmap) => {
-                    slotmap.get_value_mut(slot_key.clone()).unwrap().update(event, public_data)
-                },
-                WindowType::WindowTwo(slotmap) => {
-                    slotmap.get_value_mut(slot_key.clone()).unwrap().update(event, public_data)
-                },
+    pub fn handle_event(
+        &mut self,
+        event: &UIEvent,
+        public_data: &mut PublicDataCollection<DataTypeKey, DataType>,
+    ) {
+        for (gui_type, slot_key) in self.active_window_collection.iter() {
+            update_window(&mut self.window_collection, *gui_type, *slot_key, event, public_data);
+        }
+    }
+
+    pub fn update(&mut self, public_data: &mut PublicDataCollection<DataTypeKey, DataType>) {
+        /* Nothing yet - The UIEvent to be sent to the GUI containers is going to be created here */
+    }
+
+    pub fn render(
+        &mut self,
+        gui_rects: &mut GUIRects,
+        encoder: &mut rwge::wgpu::CommandEncoder,
+        system_bind_group: &wgpu::BindGroup,
+        public_data: &mut PublicDataCollection<DataTypeKey, DataType>,
+    ) {
+        {
+            let window_count = self.active_window_collection.len();
+            let event = UIEvent::Render(gui_rects);
+            for forward_index in 0..window_count {
+                let window_index = window_count - 1 - forward_index;
+                let (gui_type, slot_key) = self.active_window_collection[window_index];
+                update_window(&mut self.window_collection, gui_type, slot_key, &event, public_data);
             }
+        }
+
+        if let DataType::Base(EngineDataType::RenderTexture(render_texture_slotmap)) = public_data
+            .collection
+            .get(&DataTypeKey::Base(EngineDataTypeKey::RenderTexture))
+            .expect("No Render Texture collection was found")
+        {
+            let color_rt = render_texture_slotmap
+                .get_value(gui_rects.render_texture.color_texture_key.key)
+                .expect("Color Render Texture not found");
+            let mask_rt = render_texture_slotmap
+                .get_value(gui_rects.render_texture.mask_texture_key.key)
+                .expect("Mask Render Texture not found");
+            rwge::gui::rect_ui::render_pass::render_gui(
+                encoder,
+                &gui_rects,
+                system_bind_group,
+                &color_rt.texture_view,
+                &mask_rt.texture_view,
+            );
+        } else {
+            panic!("Render texture slotmap not found or Color / Mask RT not found");
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
-use core::any::TypeId;
-
 pub use rwge::gui::rect_ui::system::GUIRects;
 mod gui_system;
 use gui_system::GUISystem;
-use rwge::{Engine, RenderTextureSlotmap, slotmap::slotmap::Slotmap, gui::rect_ui::event::UIEvent};
+use rwge::{
+    color::RGBA,
+    entity_component::{EngineDataTypeKey, PublicDataCollection},
+    slotmap::slotmap::{SlotKey, Slotmap},
+    Engine, EngineDataType, RenderTextureSlotmap,
+};
 
-pub enum DataSlotmap{
-    Base(rwge::entity_component::EngineDataSlotmapTypes)
+pub struct Other{
+    pub value: f32
+}
+pub enum DataType {
+    Base(EngineDataType),
+    OtherType(Slotmap<Other>)
 }
 
-pub type DataSlotmaps = Slotmap<DataSlotmap>;
+impl From<EngineDataType> for DataType {
+    fn from(key: EngineDataType) -> Self {
+        DataType::Base(key)
+    }
+}
 
+#[derive(Hash, PartialEq, Eq)]
+pub enum DataTypeKey {
+    Base(EngineDataTypeKey),
+}
 
+impl From<EngineDataTypeKey> for DataTypeKey {
+    fn from(key: EngineDataTypeKey) -> Self {
+        DataTypeKey::Base(key)
+    }
+}
+
+pub struct DataKey {
+    pub map_key: DataTypeKey,
+    pub key: SlotKey,
+}
 
 struct Game {
+    gui_rects: GUIRects,
     gui_system: GUISystem,
-    public_data: DataSlotmaps
+    public_data: PublicDataCollection<DataTypeKey, DataType>,
 }
 
 impl Game {
@@ -31,11 +57,18 @@ impl Game {
             &mut render_texture_slotmap,
         );
 
-        let gui_system = GUISystem::new(gui_rects);
+        let gui_system = GUISystem::new();
 
-        let public_data = DataSlotmaps::new_with_capacity(20);
-
-        Self { gui_system, public_data }
+        let mut public_data = PublicDataCollection::<DataTypeKey, DataType>::new();
+        public_data.collection.insert(
+            EngineDataTypeKey::RenderTexture.into(),
+            EngineDataType::RenderTexture(render_texture_slotmap).into(),
+        );
+        Self {
+            gui_rects,
+            gui_system,
+            public_data,
+        }
     }
 }
 
@@ -50,7 +83,7 @@ impl rwge::Runtime for Game {
         println!("Handle event queue log");
         for event in event_queue {
             let close_event_handled = rwge::default_close_event_handler(event, exit_event_loop);
-            if !close_event_handled{
+            if !close_event_handled {
                 let gui_event = rwge::gui::rect_ui::event::default_event_transformation(event);
                 if let Some(e) = gui_event {
                     self.gui_system.handle_event(&e, &mut self.public_data);
@@ -59,23 +92,35 @@ impl rwge::Runtime for Game {
         }
     }
 
-    fn update(&mut self, engine: &rwge::Engine) {}
+    fn update(&mut self, engine: &rwge::Engine) {
+        self.gui_system.update(&mut self.public_data);
+    }
 
     fn render(
         &mut self,
         engine: &mut rwge::Engine,
         screen_view: &rwge::wgpu::TextureView,
         encoder: &mut rwge::wgpu::CommandEncoder,
-    ) -> bool {
-        println!("Render log");
-        false
+    ) {
+        rwge::render_system::utils::texture_utils::clear_render_targets(
+            encoder,
+            screen_view,
+            RGBA::GREEN.into(),
+            None,
+            None,
+            None,
+        );
+
+        
+
+        self.gui_system.render(&mut self.gui_rects, encoder, &engine.system_bind_group, &mut self.public_data);
     }
 
     fn frame_end<F>(&mut self, exit_event_loop: &mut F)
     where
         F: FnMut() -> (),
     {
-        println!("Frame end log");
+        //println!("Frame end log");
         //exit_event_loop();
     }
 
@@ -90,6 +135,4 @@ fn main() {
     let game = Game::new(&engine);
 
     rwge::start_engine_loop(engine, game, event_loop);
-
-    println!("The engine stopped!!");
 }
